a9d38a6cc27a68710e755e49298e0478bb9b435b
chduch67 <162667350+chduch67@users.noreply.github.com>
2024-04-12 14:14:47 +0900

    Add files via upload

--- 0405/App.js
+++ 0405/App.js
@@ -1,18 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './classComp/Header';
-import Main from './classComp/Main';
-import Footer from './classComp/Footer';
-import MyComponent from './MyComponent';
+import React from "react";
+import Header from "./classComp/Header";
+import Footer from "./classComp/Footer";
+import Main from "./classComp/Main";
 
 function App() {
   return (
-   <div>
-   <MyComponent name="HTML" />
-   <MyComponent name="React" />
-   <MyComponent name="Javascript" />
-   </div>
-  )
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
-
 export default App;
